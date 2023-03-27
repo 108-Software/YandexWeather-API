5e6a9369171bdc9289a798666ce9b8560461a0f5
108-Software <75811746+108-Software@users.noreply.github.com>
2023-03-27 16:51:04 +0300

    Add files via upload

---
+++ request.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type json_ struct {
+	Temp int `json:"temp"`
+}
+
+type json2 struct {
+	Fact json_ `json:"fact"`
+}
+
+func main() {
+	url := fmt.Sprintf("https://api.weather.yandex.ru/v2/forecast?lan=55.60163&lon=37.34665")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("X-Yandex-API-Key", "baea74df-7a43-4d00-9b5d-823f9be09c62")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println(string(body))
+
+	var requ json2
+	err = json.Unmarshal(body, &requ)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Температура сейчас: %d\n", requ.Fact.Temp)
+
+}
